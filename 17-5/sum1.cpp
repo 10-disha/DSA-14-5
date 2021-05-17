48dc8302bc950aab4ce05707259c00bb583eb2d8
10-disha <jayswaldisha5@gmail.com>
2021-05-17 10:26:13 +0530

    first commit

---
+++ 17-5/sum1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum;
+
+    cout << "take n";
+    cin >> n;
+
+    sum = n * (n + 1) / 2;
+    cout << "sum is " << sum;
+
+    return 0;
+    
+}
