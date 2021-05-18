1c4b93bb2559a8b44e9e07119a1158785f37192e
10-disha <jayswaldisha5@gmail.com>
2021-05-18 16:01:48 +0530

    check prime

---
+++ 18-5/Prime2.cpp
@@ -0,0 +1,26 @@
+//efficient mehtod
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i * i < n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main(void)
+{
+    int number = 3;
+    cout << isPrime(number);
+    return 0;
+}
+
+//time c(omplexity-- Big O(root(n)))
