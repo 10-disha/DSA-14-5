d8e39ad5467f01891641d4eadcaa4727a12418b2
10-disha <jayswaldisha5@gmail.com>
2021-05-18 09:01:06 +0530

    count digit

--- 17-5/countdig3.cpp
+++ 17-5/countdig3.cpp
@@ -1,14 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include <math.h>
 using namespace std;
 
 int countDigit(long long n)
 {
-    if (n == 0)
-    {
-        return 0;
-    }
-
-    return 1 + countDigit(n / 10);
+    return floor(log10(n) + 1);
 }
 
 int main(void)
