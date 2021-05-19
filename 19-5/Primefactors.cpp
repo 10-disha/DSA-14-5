3e5e66dcfa1a94684991f336bdabe04edbcec1dc
10-disha <jayswaldisha5@gmail.com>
2021-05-19 17:43:21 +0530

    prime factor

---
+++ 19-5/Primefactors.cpp
@@ -0,0 +1,13 @@
+// #include <iostream>
+// using namespace std;
+
+// Printprimenumberes(int num){
+
+// }
+
+// int main(void)
+// {
+//     int number = 334;
+//     cout << Printprimenumbers(number);
+//     return 0;
+// }
