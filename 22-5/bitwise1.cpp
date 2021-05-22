ffb06cd049952a34bd3af5bdcff2b358ab04efe8
10-disha <jayswaldisha5@gmail.com>
2021-05-22 14:32:12 +0530

    bit manip

--- 22-5/bitwise1.cpp
+++ 22-5/bitwise1.cpp
@@ -1,3 +1,5 @@
+//and or xor operators
+
 #include <iostream>
 #include <limits.h>
 
@@ -5,4 +7,11 @@ using namespace std;
 
 int main()
 {
+    int x = 3, y = 6;
+
+    cout << (x & y) << endl;
+    cout << (x | y) << endl;
+    cout << (x ^ y) << endl;
+
+    return 0;
 }
---
+++ 22-5/bitwise2.cpp
@@ -0,0 +1,15 @@
+//signed not operator
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 1;
+    cout << (~x) << endl;
+
+    int y = 5;
+    cout << (~y) << endl;
+
+    return 0;
+}
