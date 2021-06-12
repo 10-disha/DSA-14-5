a2d0faef6fe465954acda4f323a1adb4c6221625
10-disha <jayswaldisha5@gmail.com>
2021-06-12 10:25:50 +0530

    recursion

---
+++ 12-6/recur5.cpp
@@ -0,0 +1,17 @@
+//sum of digits using recurrsion
+#include<iostream>
+using namespace std;
+
+int sum(int n){
+  if(n<=9)
+  return n;
+
+  return sum(n/10)+n%10;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<sum(n);
+    return 0;
+}
