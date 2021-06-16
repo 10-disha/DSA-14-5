0b4203e02ee23db1cd842b327c91875f0d83582b
10-disha <jayswaldisha5@gmail.com>
2021-06-16 10:46:19 +0530

    arrays

--- 16-6/array2.cpp
+++ 16-6/array2.cpp
@@ -4,9 +4,29 @@
 using namespace std;
 
 int main(){
-    int array[100],position,value,n;
-    
+    int array[100],position,value,n,c;
+    cout<<"enter the no. of elements"<<endl;
+    cin>>n;
 
+    cout<<"enter the elements"<<endl;
+    for(c=0;c<n;c++){
+        cin>>array[c];
+    }
 
-    return 0;
+    cout<<"enter position where element to be inserted"<<endl;
+    cin >>position;
+
+    cout<<"enter value of an element to be inserted"<<endl;
+    cin>>value;
+
+    for(c=n-1; c>=position -1;c--){
+      array[c+1]=array[c];
+    }
+    array[position-1]=value;
+
+    cout<<"resultant array:"<<endl;
+     for(c=0;c<n;c++){
+        cout<<array[c]<<endl;
+    }
+   return 0;
 }
