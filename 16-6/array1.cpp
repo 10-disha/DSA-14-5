c1c235dd9b1ce656512df9bf7d81c5babcf70e4d
10-disha <jayswaldisha5@gmail.com>
2021-06-16 10:00:24 +0530

    array1

---
+++ 16-6/array1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int search(int arr[],int n,int x){
+for(int i=0;i<=n;i++){
+    if(arr[i]==x)
+    return i;
+}
+
+return -1;
+}
+
+int main(){
+
+   
+    int arr[]={20,10,3,4,5},x=5;
+
+    cout<<search(arr,5,x);
+   
+}
