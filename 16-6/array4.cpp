a86767e72413211acf06f6c93bc031bd9204b355
10-disha <jayswaldisha5@gmail.com>
2021-06-17 08:47:45 +0530

    arrays

--- 16-6/array4.cpp
+++ 16-6/array4.cpp
@@ -1,2 +1,28 @@
 //deletion of an element in an array 
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int delete(int arr[],int n,int x){
+    
+}
+
+
+int main(){
+    int arr[]={3,4,66,12,5,6},x=12,n=6;
+    cout<<"before deletion"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<"";
+    }
+    cout<<endl;
+
+    n=delete(arr[],x,n);
+    cout<<"after deletion"<<endl;
+     for(int i=0;i<n;i++){
+        cout<<arr[i]<<"";
+    }
+    
+}
+
+
 
