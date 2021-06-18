88d66fd5ba455681037a4dd31fe09bacaa28ce9f
10-disha <jayswaldisha5@gmail.com>
2021-06-18 11:21:06 +0530

    ararys

--- 18-6/array3.cpp
+++ 18-6/array3.cpp
@@ -1,2 +1,36 @@
 //naive method to find second largest element.
-#include
+#include<iostream>
+using namespace std;
+
+int getSecond(int arr[],int n){
+    int largest=getLargest(arr,n);
+    int res=-1;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]!=arr[largest]){
+            if(res==-1)
+            res=i;
+
+            else if(arr[i]>arr[res])
+            res=i;
+        }
+    }
+
+    return res;
+}
+
+int getLargest(int arr[],int n){
+  int largest=0;
+   for(int i=0;i<n;i++){
+       if(arr[largest] < arr[i])
+       largest=i;
+   }
+
+   return largest;
+}
+
+int main(){
+    int arr[]={2,4,55,33},n=4;
+    cout<<getSecond(arr,n);
+    return 0;
+}
