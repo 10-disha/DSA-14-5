f3d034b19f837ae5924876b5cf3077b7c60da2f5
10-disha <jayswaldisha5@gmail.com>
2021-08-24 08:20:38 +0530

    searching

---
+++ 24-8/search2.cpp
@@ -0,0 +1,15 @@
+//recursive solution
+#include<iostream>
+using namespace std;
+
+int bSearch(int arr[],int low,int high,int x){
+    
+}
+
+int main(){
+    int arr[]={10,20,30,40,50}, n=5;
+    int x=30;
+
+    cout<<bSearch(arr,low,high,x);
+    return 0;
+}
