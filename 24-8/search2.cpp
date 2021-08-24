89e120d90784de86713a36b75b46bf344d2bfd74
10-disha <jayswaldisha5@gmail.com>
2021-08-24 08:24:08 +0530

    search

--- 24-8/search2.cpp
+++ 24-8/search2.cpp
@@ -3,13 +3,26 @@
 using namespace std;
 
 int bSearch(int arr[],int low,int high,int x){
-    
+            
+            if(low>high)
+            return -1;
+
+            int mid=(low+high)/2;
+
+            if(arr[mid]==x)
+            return mid;
+
+            else if(arr[mid]>x)
+            return bSearch(arr,low,mid-1,x);
+
+            else
+            return bSearch(arr,mid+1,high,x);
 }
 
 int main(){
-    int arr[]={10,20,30,40,50}, n=5;
-    int x=30;
+    int arr[]={10,10,20,30,40,50}, n=5;
+    int x=10;
 
-    cout<<bSearch(arr,low,high,x);
+    cout<<bSearch(arr,0,n-1,x);
     return 0;
 }
